f3c5b62ec22a448309145036a9dcd5c5041b06a8
lucky-blessed <blessednkwor7@gmail.com>
2024-09-18 15:36:09 +0100

    user model.

---
+++ backend/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: true, unique: true },
+        email: { type: String, require: true, unique: true },
+        password: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+// Hash the password before saving the user
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.method.matchPassword = async function (enteredPassword)  {
+    return await bcrypt.compare(enteredPassword, this.password);    
+};
+
+module.exports = mongoose.model('UserModel', UserSchema);
