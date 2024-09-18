ff284f3e9cbc43622a77df71b726c41f3f5e590f
lucky-blessed <blessednkwor7@gmail.com>
2024-09-18 12:26:33 +0100

    ID parameters validation

---
+++ backend/middleware/valid-id-param.js
@@ -0,0 +1,11 @@
+const isValidObjectId = require('../utils/is-valid-id');
+
+function validIdParam(req, res, next) {
+    const id = req.param.id;
+
+    if (!isValidObjectId(id)) return res.status(400).json({ error: 'Invalid ID format.'});
+
+    next();
+}
+
+module.exports = validIdParam;
