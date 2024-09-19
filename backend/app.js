967006aae0d90ecaea8d27106a80266555cb6d7c
lucky-blessed <blessednkwor7@gmail.com>
2024-09-19 17:56:26 +0100

    Hot fix

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require("./routes/auth");
 const eventRoutes = require("./routes/events");
-const rsvpRoutes = require("/routes/rsvp");
+const rsvpRoutes = require("./routes/rsvp");
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 
 const PORT = process.env.PORT || 5000;
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI, {})
     .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get("/", (req, res) => {
-    res.send("Hello MERN Stack");
+    res.send("Hello MERN Stack!");
 });
 
 
