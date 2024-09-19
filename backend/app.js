7aa1435c1816acfe8617d59b51e8fd5fd2a55acc
lucky-blessed <blessednkwor7@gmail.com>
2024-09-19 13:59:36 +0100

    app update.

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const authRoutes = require("./routes/auth");
+const eventRoutes = require("./routes/events");
+const rsvpRoutes = require("/routes/rsvp");
 
 dotenv.config();
 
@@ -10,15 +13,18 @@ const app = express();
 app.use(cors());
 app.use(express.json);
 
+
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGODB_URI, {})
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
-app.get('/', (req, res) => {
-    res.send('Hello MERN Stack');
+app.get("/", (req, res) => {
+    res.send("Hello MERN Stack");
 });
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
