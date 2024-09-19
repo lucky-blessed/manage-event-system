04d351a2e7315e529887fb5c5ae23da8d6c31af9
lucky-blessed <blessednkwor7@gmail.com>
2024-09-19 21:21:13 +0100

    Creating RSVP

---
+++ frontend/event-dashboard/src/components/RSVP.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function RSVP() {
+    const [eventId, setEventId] = useState('');
+
+    const handleRSVP = () => {
+        axios.post(`http://localhost:5000/api/rsvp/${eventId}`)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error("Error creating RSVP!", error);
+            });
+    };
+
+    return (
+        <div>
+            <input type="text" value={eventId} onChange={(e) => setEventId(e.target.value)} placeholder="Event ID" required />
+            <button onClick={handleRSVP}>RSVP</button>
+        </div>
+    );
+}
+
+export default RSVP;
